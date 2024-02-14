d581ff34bfd87caa44e9ef7e31609b0cfa23cef3
Submarinetwentythousand <154326933+LeaguesUndertheSea@users.noreply.github.com>
2024-02-14 22:11:17 +0800

    practice_007
    pravtice_007

---
+++ practice_004/pratice_004.c
@@ -0,0 +1,453 @@
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	float temperature = 36.5f;
+//
+//	float humidity = 48.3f;
+//
+//	float speed_of_sound = 32.5e2f;
+//
+//	float length = 12.34f, width = 23.45f, height = 34.56f;
+//
+//	printf("Temperature : %f\n", temperature);
+//	printf("Humidity : %f\n", humidity);
+//	printf("Speed of sound : %f\n", speed_of_sound);
+//	printf("Dimensions : %f x %f x %f\n", length, width, height );
+//
+//	double sNum = 100.3;
+//	printf("sNum = %lf\n", sNum);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	float temperature = 36.5f;
+//
+//	float humidity = 48.3f;
+//
+//	float speed_of_sound = 32.5e2f;
+//
+//	float length = 12.34f, width = 23.45f, height = 34.56f;
+//
+//	printf("Temperature : %f\n", temperature);
+//	printf("Humidity : %f\n", humidity);
+//	printf("Speed of sound : %f\n", speed_of_sound);
+//	printf("Dimensions : %f x %f x %f\n", length, width, height);
+//
+//	double sNum = 100.3;
+//	printf("sNum : %lf\n", sNum);
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	float num = 123.456f;
+//
+//	printf("Using %%f: %f\n", num);
+//
+//	printf("Using %%e: %e\n", num);
+//	printf("Using %%E: %E\n", num);
+//
+//	printf("Using %%a: %a\n", num);
+//	printf("Using %%A: %A\n", num);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	float num = 123.456f;
+//
+//	printf("Using %%f: %f\n", num);
+//
+//	printf("Using %%e: %e\n", num);
+//	printf("Using %%E: %E\n", num);
+//
+//	printf("Using %%a: %a\n", num);
+//	printf("Using %%A: %A\n", num);
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//#include <float.h>
+//
+//int main(void)
+//{
+//	float max_float = FLT_MAX;
+//
+//	float over_float = max_float * 1000.0f;
+//
+//	float min_float = FLT_MIN;
+//
+//	float under_float = min_float / 1000.0f;
+//
+//	printf("Max_float: %f\n", max_float);
+//	printf("Over_float: %f\n", over_float);
+//	printf("Min_float: %f\n", min_float);
+//	printf("Under_float: %f\n", under_float);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <float.h>
+//
+//int main(void)
+//{
+//	float max_float = FLT_MAX;
+//
+//	float over_float = max_float * 1000.0f;
+//
+//	float min_float = FLT_MIN;
+//
+//	float under_float = min_float / 1000.0f;
+//
+//	printf("Max_float: %f\n", max_float);
+//	printf("Over_float: %f\n", over_float);
+//	printf("Min_float: %f\n", min_float);
+//	printf("Under_float: %f\n", under_float);
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//#include <float.h>
+//#include <math.h>
+//
+//int main(void)
+//{
+//	float positive_infinity = INFINITY;
+//	printf("Positive infinity: %f\n", positive_infinity);
+//
+//	float negative_infinity = -INFINITY;
+//	printf("Negative infinity: %f\n", negative_infinity);
+//
+//	float num = 1.0f;
+//	float infinity = num / 0.0f;
+//	printf("Infinity: %f\n", infinity);
+//
+//	float zero1 = 0.0f;
+//	float zero2 = 0.0f;
+//	float nan = zero1 / zero2;
+//	printf("1.0 / 0.0: %f\n", nan);
+//
+//	float number = sqrt(4);
+//	printf("Sqrt(4) = %f\n", number);
+//
+//	float negative_num = sqrt(-1.0f);
+//	printf("Sqrt(-1.0f) = %f\n", negative_num);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <float.h>
+//#include <math.h>
+//
+//int main(void)
+//{
+//	float positive_infinity = INFINITY;
+//	printf("Positive infinity: %f\n", positive_infinity);
+//
+//	float negative_infinity = -INFINITY;
+//	printf("Negative infinity: %f\n", negative_infinity);
+//
+//	float num = 1.0f;
+//	float infinity = num / 0.0f;
+//	printf("Infinity: %f\n", infinity);
+//
+//	float zero1 = 0.0f;
+//	float zero2 = 0.0f;
+//	float nan = zero1 / zero2;
+//	printf("Nan = %f\n", nan); 
+//
+//	float number = sqrt(4);
+//	printf("Sqrt(4) = %f\n", number);
+//
+//	float negative_num = sqrt(-1.0);
+//	printf("Negative number: %f\n", negative_num);
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	float number = 3.15f;
+//	printf("1-> : %.1f\n", number);
+//
+//	number = 3.20f;
+//	printf("2-> : %.1f\n", number);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	float number = 3.15f;
+//	printf("1->: %.1f\n", number);
+//
+//	number = 3.20f;
+//	printf("2->: %.1f\n", number);
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	double num1 = 123.456;
+//	double num2 = 765.432;
+//
+//	double num = num1 + num2;
+//	double difference = num1 - num2;
+//	double product = num1 * num2;
+//	double quotient = num1 / num2;
+//	long double ld_val = 123.234234;
+//
+//	printf("Num: %f\n", num);
+//	printf("Difference: %f\n", difference);
+//	printf("Product: %f\n", product);
+//	printf("Quotient: %f\n", quotient);
+//	printf("Long double: %f\n", ld_val);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	double num1 = 123.456;
+//	double num2 = 765.432;
+//
+//	double num = num1 + num2;
+//	double difference = num1 - num2;
+//	double product = num1 * num2;
+//	double quotient = num1 / num2;
+//	long double ld_val = 123.34432;
+//
+//	printf("Num: %f\n", num);
+//	printf("Difference: %f\n", difference);
+//	printf("Product: %f\n", product);
+//	printf("Quotient: %f\n", quotient);
+//	printf("Long double: %f\n", ld_val);
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//#include <float.h>
+//
+//int main(void)
+//{
+//	float float_num = 1.0 / 3.0;
+//	double double_num = 1.0 / 3.0;
+//
+//	printf("Float precision : %.20f\n", float_num);
+//	printf("Double precision : %.20f\n", double_num);
+//
+//	printf("Defined max precision for float : %d\n", FLT_DIG);
+//	printf("Defined max precision for double : %d\n", DBL_DIG);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <float.h>
+//
+//int main(void)
+//{
+//	float float_num = 1.0 / 3.0;
+//	double double_num = 1.0 / 3.0;
+//
+//	printf("Float number : %.20f\n", float_num);
+//	printf("Double number : %.20f\n", double_num);
+//
+//	printf("Defined max precision for float : %d\n", FLT_DIG);
+//	printf("Defined max predision for double : %d\n", DBL_DIG);
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	char mych = 'A';
+//
+//	printf("Mych = %c\n", mych);
+//
+//	int mychar = 97;
+//	printf("Mychar = %c\n", mychar);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	char mych = 'A';
+//	printf("Mych = %c\n", mych);
+//
+//	int mychar = 97;
+//	printf("Mychar = %c\n", mychar);
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	printf("A");
+//	printf("%\033[2J");
+//	printf("\033[%d;%dH\n", 3, 3);
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	printf("第一行文本\n\t，第二行文本\n\t\t，第三行文本\n");
+//	printf("语言：\"C语言\"\n");
+//	printf("转移序列练习\t");
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//#include <stdbool.h>
+//
+//int main(void)
+//{
+//	bool is_game_over = false;
+//	bool is_game_won = true;
+//
+//	printf("Is_game_over : %d\n", is_game_over);
+//	printf("Is_game_over : %d\n", is_game_won);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <stdbool.h>
+//
+//int main(void)
+//{
+//	bool is_game_over = false;
+//	bool is_game_won = true;
+//
+//	printf("Is_game_over : %d\n", is_game_over);
+//	printf("Is_game_won : %d\n", is_game_won);
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//#define PI 3.14
+//
+//int main(void)
+//{
+//	const int MAX_USERS = 100;
+//	printf("MAX_USERS : %d\n", MAX_USERS);
+//	printf("PI : %lf\n", PI);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#define PI 3.14
+//
+//int main(void)
+//{
+//	const int MAX_USERS = 100;
+//
+//	printf("MAX_USERS : %d\n", MAX_USERS);
+//	printf("PI : %lf\n", PI);
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//#include <stdint.h>
+//#include <inttypes.h>
+//
+//int main(void)
+//{
+//	uint32_t apple_box = 5;
+//	uint32_t orange_box = 10;
+//
+//	printf("苹果盒子里有 %" PRIu32 "个苹果\n", apple_box);
+//	printf("橘子盒子里有 %" PRIu32 "个橘子\n", orange_box);
+//
+//	uint32_t total_fruit = apple_box + orange_box;
+//
+//	printf("盒子里总共有 %" PRIu32 "\n", total_fruit);
+//	
+//	uint32_t x, y, z;
+//
+//	x = y = z = 10;
+//
+//	printf("x: %" PRIu32 "\n", x);
+//	printf("y: %" PRIu32 "\n", y);
+//	printf("z: %" PRIu32 "\n", z);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <stdint.h>
+//#include <inttypes.h>
+//
+//int main(void)
+//{
+//	uint32_t apple_box = 5;
+//	uint32_t orange_box = 10;
+//
+//	printf("苹果盒子里有 %" PRIu32 "个苹果\n", apple_box);
+//	printf("橘子盒子里有 %" PRIu32 "个橘子\n", orange_box);
+//
+//	uint32_t total_fruits = apple_box + orange_box;
+//	printf("盒子里总共有 %" PRIu32 "个水果\n", total_fruits);
+//
+//	uint32_t x, y, z;
+//	x = y = z = 10;
+//
+//	printf("x: %" PRIu32 "\n", x);
+//	printf("y: %" PRIu32 "\n", y);
+//	printf("z: %" PRIu32 "\n", z);
+//
+//	return 0;
+//}
+
+
